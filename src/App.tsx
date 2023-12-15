39f2213ac8dacf76cd0fddecd4b31cfec455d812
Joseph Lander <josephclander@gmail.com>
2023-12-15 22:28:08 +0000

    Add basic markup and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ function App() {
   return (
     <>
       <h1>Todo App</h1>
+      <h2>Tasks</h2>
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+        <li className="complete">Four</li>
+      </ul>
+      <button>Add Task</button>
     </>
   );
 }
