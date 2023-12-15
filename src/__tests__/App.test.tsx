9d9c307938db4d7a7bdf0eefdaf48c8fe9e58e08
Joseph Lander <josephclander@gmail.com>
2023-12-15 22:12:43 +0000

    Add cleanup to tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,11 @@
-import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it, expect, afterEach } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/react';
 import App from '../App';
 
 describe('Renders main page correctly', async () => {
+  afterEach(() => {
+    cleanup();
+  });
   it('should render the main page correctly', async () => {
     render(<App />);
     const h1 = await screen.queryByText('Todo App');
