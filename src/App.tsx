92f2822a9bc683d52c53f11098c690c8345a2579
Joseph Lander <josephclander@gmail.com>
2023-12-16 19:23:18 +0000

    Refine styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <main>
       <h1>Todo App</h1>
-      <h2>Tasks</h2>
-      <ul>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
-        <li className="complete">Four</li>
-      </ul>
-      <button>Add Task</button>
-    </>
+      <section>
+        <h2>Tasks</h2>
+        <ul>
+          <li className="task__container">
+            <button className="delete">X</button><span className="task">One</span>
+          </li>
+          <li className="task__container">
+            <button className="delete">X</button><span className="task">Two</span>
+          </li>
+          <li className="task__container">
+            <button className="delete">X</button><span className="task">Three</span>
+          </li>
+          <li className="task__container">
+            <button className="delete">X</button><span className="task complete">Four</span>
+          </li>
+        </ul>
+        <button className="submit">Add Task</button>
+      </section>
+    </main>
   );
 }
 
